4f44a77307b8d7f9714774ef4bb9ed44fecc1258
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2019-01-04 14:25:04 +0900

    Fix indent

--- uart_loopback.v
+++ uart_loopback.v
@@ -20,21 +20,21 @@ module uart_loopback(
   //------------------------
 
 	uart #(
-			.baud_rate(9600),                 // default is 9600
-			.sys_clk_freq(50000000)           // default is 100000000
+    .baud_rate(9600),                 // default is 9600
+    .sys_clk_freq(50000000)           // default is 100000000
 	)
 	instance_name(
-			.clk(clk),                        // The master clock for this module
-			.rst(rst),                        // Synchronous reset
-			.rx(rx),                          // Incoming serial line
-			.tx(tx),                          // Outgoing serial line
-			.transmit(flag),                  // Signal to transmit
-			.tx_byte(data),                   // Byte to transmit
-			.received(flag),                  // Indicated that a byte has been received
-			.rx_byte(data),                   // Byte received
-			.is_receiving(),                  // Low when receive line is idle
-			.is_transmitting(),               // Low when transmit line is idle
-			.recv_error()                     // Indicates error in receiving packet.
+    .clk(clk),                        // The master clock for this module
+    .rst(rst),                        // Synchronous reset
+    .rx(rx),                          // Incoming serial line
+    .tx(tx),                          // Outgoing serial line
+    .transmit(flag),                  // Signal to transmit
+    .tx_byte(data),                   // Byte to transmit
+    .received(flag),                  // Indicated that a byte has been received
+    .rx_byte(data),                   // Byte received
+    .is_receiving(),                  // Low when receive line is idle
+    .is_transmitting(),               // Low when transmit line is idle
+    .recv_error()                     // Indicates error in receiving packet.
 	);
 
 endmodule
